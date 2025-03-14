f4e9e3729ae43daeb6c202b196ed0817e9767002
Rob Doneux <robertdoneux@gmail.com>
2025-03-14 19:08:31 +0000

    add Azure resources: create Resource Group, Virtual Network, and Subnet

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";
+import { ResourceGroup } from "@pulumi/azure/core";
+import { Subnet, VirtualNetwork } from "@pulumi/azure/network";
 
+// Create Resource Group
+const resourceGroup = new ResourceGroup("shared-network-rg");
+
+// Create Virtual Network
+const vnet = new VirtualNetwork("shared-vnet", {
+  resourceGroupName: resourceGroup.name,
+  location: resourceGroup.location,
+  addressSpaces: ["10.0.0.0/16"],
+});
+
+// Create a shared Subnet
+const subnet = new Subnet("shared-subnet", {
+  resourceGroupName: resourceGroup.name,
+  virtualNetworkName: vnet.name,
+  addressPrefixes: ["10.0.1.0/24"],
+});
+
+// Export values for microservices to use
+export const vnetId = vnet.id;
+export const subnetId = subnet.id;
